72950170588dceedbd5789be8eea655a89c46b6d
Jiawen-Ou <jiawenou2016@u.northwestern.edu>
2016-05-17 23:17:56 -0500

    change to input

--- auto_dropdown_item.js
+++ auto_dropdown_item.js
@@ -8,40 +8,34 @@ test.controller('MyController', function($scope) {
 				kinect: '',
 			};
 	$scope.buildings = [{
-				Id: ': #1',
+				Id: '1',
 				Name: 'One',
 			},  {
-				Id: ': #2',
+				Id: '2',
 				Name: 'Two',
 			},  {
-				Id: ': #3',
+				Id: '3',
 				Name: 'Three',
 			// }, 	{
 			// 	Id: ': #4',
 			// 	Name: 'Four',
 			}];
 	$scope.rooms = [{
-				Id: ': #1',
+				Id: '1',
 				Name: 'One',
 			},  {
-				Id: ': #2',
+				Id: '2',
 				Name: 'Two',
 			// }, 	{
 			// 	Id: ': #3',
 			// 	Name: 'Three',
 			}];
 	$scope.kinects = [{
-				Id: ': #1',
+				Id: '1',
 				Name: 'One',
 			},  {
-				Id: ': #2',
+				Id: '2',
 				Name: 'Two',
-			},  {
-				Id: ': #3',
-				Name: 'Three',
-			}, 	{
-				Id: ': #4',
-				Name: 'Four',
 			}];
 });
 
